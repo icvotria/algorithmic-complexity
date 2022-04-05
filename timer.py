b0fab8f5353ae328c9203a962c233eeb6d960407
unknown <icvotria@gmail.com>
2022-04-05 15:48:53 +0100

    started bubble sort algo

--- timer.py
+++ timer.py
@@ -1,19 +1,23 @@
 import time 
 
 def bubble_sort(lst):
-  length = len(lst)
+  length = len(lst) - 1
   for i in range(length):
-  	print(lst[i] * 128751235246)
+        for j in range(length - i):
+         if lst[i] > lst[j + 1]:
+              lst[i + 1] = lst[i]
+              lst[i] = lst[i + 1]
+  print(lst)
+            
+  	
 
 num_list = [34, 2, 76, 0, -1, 200]
 
 def timer(algo, data):
 	before = time.perf_counter()
-	print(before)
 	algo(data)
 	after = time.perf_counter()
-	print(after)
 	total = after - before
 	return total
- 
+
 print(timer(bubble_sort, num_list))
