770372ccd4aa66655ee505cda5720b94c59abcd6
unknown <icvotria@gmail.com>
2022-04-06 15:53:44 +0100

    generators for x and y values

--- plotter.py
+++ plotter.py
@@ -1,21 +1,36 @@
-# importing the required module
 import matplotlib.pyplot as plt
+from bubble_sort import bubble_sort
+from timer import timer
 
-# x axis values
-x = [i for i in range(50)]
-# corresponding y axis values
-y = [i**2 for i in x]
+def number_generator(x):
+    numbers = [i for i in range(x)]
+    num_list = numbers[::-1]
+    return num_list
 
-# plotting the points
-plt.plot(x, y)
+def input_generator():
+    values = []
+    for i in range(0, 100001, 5000):
+        values.append(number_generator(i))
+    return values
 
-# naming the x axis
-plt.xlabel('x - axis')
-# naming the y axis
-plt.ylabel('y - axis')
+def find_length(inputs):
+    return [len(i) for i in inputs]
 
-# giving a title to my graph
-plt.title('My first graph!')
+def output_generator(algo, input):
+    output = []
+    for i in input:
+        output.append(timer(algo, i))
+    return output
 
-# function to show the plot
-plt.show()
+def plotter(x_values, y_values, name):
+    plt.plot(x_values, y_values)
+    plt.xlabel('size')
+    plt.ylabel('time')
+    plt.title(name)
+    plt.show()
+
+inputs = input_generator()
+y = output_generator(bubble_sort, inputs)
+x = find_length(inputs)
+
+plotter(x, y, 'bubble sort')
