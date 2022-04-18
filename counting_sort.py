2ea8f722582581930b7343f82e831a93c91de165
unknown <icvotria@gmail.com>
2022-04-18 10:51:11 +0100

    renamed array in counting sort

--- counting_sort.py
+++ counting_sort.py
@@ -1,11 +1,11 @@
 def countingSort(arr):
-    result = []
+    count_arr = []
     x = 0
     arr_size = max(arr) + 1
-    result.extend([x]*(arr_size))
+    count_arr.extend([x]*(arr_size))
     
     for i in arr:
-        result[i] += 1
-    print(result)
+        count_arr[i] += 1
+    print(count_arr)
 
 countingSort([3, 4, 2, 2, 1, 10])
