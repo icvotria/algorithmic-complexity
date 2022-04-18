364092cec2060a300b669a97747e4e88d129f1d0
unknown <icvotria@gmail.com>
2022-04-18 11:20:11 +0100

    counting sort working

--- counting_sort.py
+++ counting_sort.py
@@ -5,10 +5,17 @@ def countingSort(arr):
     
     for i in arr:
         count_arr[i] += 1
-    print(count_arr)
     
     for i in range(1, arr_size):
         count_arr[i] += count_arr[i - 1]
-    print(count_arr)
     
-countingSort([3, 4, 2, 2, 1, 10, 8, 8, 5, 3, 6])
+    i = len(arr) - 1
+    
+    while i >= 0:
+        output[count_arr[arr[i]] - 1] = arr[i]
+        count_arr[arr[i]] -= 1
+        i -= 1
+    
+    return output
+    
+print(countingSort([3, 4, 2, 2, 1, 10, 8, 8, 5, 3, 6]))
