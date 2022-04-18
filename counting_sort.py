57efdda2ec59acb8fe3b48c3508c07634e104dca
unknown <icvotria@gmail.com>
2022-04-18 11:12:16 +0100

    cumulative count in counting sort

--- counting_sort.py
+++ counting_sort.py
@@ -1,11 +1,14 @@
 def countingSort(arr):
-    count_arr = []
-    x = 0
     arr_size = max(arr) + 1
-    count_arr.extend([x]*(arr_size))
+    count_arr = [0]*arr_size
+    output = [0] * len(arr)
     
     for i in arr:
         count_arr[i] += 1
     print(count_arr)
-
-countingSort([3, 4, 2, 2, 1, 10])
+    
+    for i in range(1, arr_size):
+        count_arr[i] += count_arr[i - 1]
+    print(count_arr)
+    
+countingSort([3, 4, 2, 2, 1, 10, 8, 8, 5, 3, 6])
